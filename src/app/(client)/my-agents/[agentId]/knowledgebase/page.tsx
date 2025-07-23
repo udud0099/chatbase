edb8bfc20ab7b260be4a2abfad113a3d45913679
udud0099 <udud0099@gmail.com>
2025-07-23 14:20:00 +0545

    client >> my-agents >> id >> Playground - basic design finish

--- src/app/(client)/my-agents/[agentId]/knowledgebase/page.tsx
+++ src/app/(client)/my-agents/[agentId]/knowledgebase/page.tsx
@@ -1,9 +1,14 @@
+import { Button } from "@/components/ui/button";
 import KnowledgeBase from "../../create/components/KnowledgeBase";
 
 export default function page() {
   return (
     <>
       <div>
+        <div className="flex justify-between gap-2 items-center mb-2">
+          <h1 className="mainH1">Knowledgebase</h1>
+          <Button variant="navButton">Re-train Agent</Button>
+        </div>
         <KnowledgeBase />
       </div>
     </>
--- src/app/(client)/my-agents/[agentId]/playground/page.tsx
+++ src/app/(client)/my-agents/[agentId]/playground/page.tsx
@@ -1,9 +1,83 @@
+import { Label } from "@/components/ui/label";
+import { Slider } from "@/components/ui/slider";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Textarea } from "@/components/ui/textarea";
 export default function page() {
   return (
     <>
       <div>
         <div>
-          <h1>playground</h1>
+          <h1 className="mainH1 mb-2">Playground</h1>
+        </div>
+        <div className="w-full h-full border border-border rounded-md p-2">
+          <div className="flex gap-4 ">
+            <div className="flex-1">
+              <div className="flex gap-2 justify-between items-center mb-4">
+                <p className="text-sm text-subText">Status</p>
+
+                <p className="text-sm text-green-600">Trained</p>
+              </div>
+              <div>
+                <div className="flex flex-col gap-y-4">
+                  <div className="grid w-full  items-center gap-3">
+                    <Label htmlFor="Title" className="text-subText text-sm">
+                      Model
+                    </Label>
+                    <Select>
+                      <SelectTrigger className=" w-full">
+                        <SelectValue placeholder="GPT-40" />
+                      </SelectTrigger>
+                      <SelectContent>
+                        <SelectGroup>
+                          <SelectLabel>GPT-40</SelectLabel>
+                          <SelectItem value="GPT-40">GPT-40</SelectItem>
+                          <SelectItem value="banana">Banana</SelectItem>
+                          <SelectItem value="blueberry">Blueberry</SelectItem>
+                          <SelectItem value="grapes">Grapes</SelectItem>
+                          <SelectItem value="pineapple">Pineapple</SelectItem>
+                        </SelectGroup>
+                      </SelectContent>
+                    </Select>
+                  </div>
+                  <div className="grid w-full  items-center gap-3">
+                    <Label htmlFor="Title" className="text-subText text-sm">
+                      Temperature
+                    </Label>
+                    <Slider defaultValue={[33]} max={100} step={1} />
+                    <div className="flex gap-2 justify-between items-center">
+                      <p className="text-subText text-sm">Reserved</p>
+                      <p className="text-subText text-sm">Creative</p>
+                    </div>
+                  </div>
+                  <div className="grid w-full  items-center gap-3">
+                    <Label htmlFor="Title" className="text-subText text-sm">
+                      Instruction
+                    </Label>
+                    <Textarea
+                      placeholder="Type your Content here."
+                      id="Content"
+                      className="min-h-[260px]"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="flex-1">
+              <Textarea
+                placeholder="Type your Content here."
+                id="Content"
+                className="min-h-[500px]"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </>
